6e0cc6b84333c758c1094c331b8d782c5244e969
Nikola <asasino.nn@gmail.com>
2019-11-30 13:42:23 +0100

    Add npm global dependencies.

--- setup-host.bat
+++ setup-host.bat
@@ -6,3 +6,7 @@ copy C:\Users\Win10\Dropbox\ACF_pro %CD%
 rem Install host global dependencies
 echo "npm install -g rimraf"
 npm install -g rimraf
+echo "npm install -g npm-run-all"
+npm install -g npm-run-all
+echo "npm install -g check-node-version"
+npm install -g check-node-version
